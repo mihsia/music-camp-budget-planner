c7255bf8a9627746cd5db1fbf03ae7d3d98ee18a
Eric Hsia <eric@Erics-MacBook-Pro.local>
2026-06-17 16:03:47 +0800

    Configure Firebase auth project

--- auth-config.js
+++ auth-config.js
@@ -1,6 +1,9 @@
 window.MUSIC_BUDGET_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  appId: "",
+  apiKey: "AIzaSyClXlXdWOfbFOJYjHgiDCHUF-6RCmPIvIE",
+  authDomain: "music-camp-budget-planner.firebaseapp.com",
+  projectId: "music-camp-budget-planner",
+  storageBucket: "music-camp-budget-planner.firebasestorage.app",
+  messagingSenderId: "254268165518",
+  appId: "1:254268165518:web:b69e471bccc5771dfb28df",
+  measurementId: "G-9PWLLVR8N3",
 };
